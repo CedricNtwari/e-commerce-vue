60d96752d252b0918f0dad48de0318449fc3322d
CedricNtwari <cedric.ntwari@powercoders.org>
2024-10-30 15:56:06 +0100

    feat: Set up ShopProductList and CartProductList with grid

---
+++ src/components/Shop/ShopProductList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import ShopProduct from './ShopProduct.vue'
+</script>
+
+<template>
+  <div class="grid p-20">
+    <ShopProduct />
+    <ShopProduct />
+    <ShopProduct />
+    <ShopProduct />
+    <ShopProduct />
+    <ShopProduct />
+    <ShopProduct />
+    <ShopProduct />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-auto-rows: 300px;
+  gap: 20px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: 480px) {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
