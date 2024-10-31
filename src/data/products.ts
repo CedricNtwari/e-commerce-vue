7bceff38a2869614d8d36ca425b263ff37cde79c
CedricNtwari <cedric.ntwari@powercoders.org>
2024-10-31 08:26:10 +0100

    feat(data): add initial product data with image URLs, title, description, and price

---
+++ src/data/products.ts
@@ -0,0 +1,73 @@
+export default [
+  {
+    id: 1,
+    image:
+      'https://media.ldlc.com/r1600/ld/products/00/05/82/01/LD0005820198_1.jpg',
+    title: 'Macbook Pro',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 1500,
+  },
+  {
+    id: 2,
+    image:
+      'https://p4-ofp.static.pub//fes/cms/2024/03/27/7p53rmw90imz3bm3yaospkfkh1t274701933.png',
+    title: 'Levono Pro',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 2300,
+  },
+  {
+    id: 3,
+    image:
+      'https://i5.walmartimages.com/seo/ASUS-Zenbook-14-inch-OLED-WUXGA-Touch-Laptop-AMD-Ryzen-7-8840HS-16GB-RAM-512GB-SSD-Jade-Black_0ff75495-29af-40cb-911d-1362535998ba.6749d98cef10f8509c64aa7701f646df.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF',
+    title: 'ASUS-Zenbook',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 1200,
+  },
+  {
+    id: 4,
+    image: 'https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_SX679_.jpg',
+    title: 'Acer Nitro 5',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 4500,
+  },
+  {
+    id: 5,
+    image:
+      'https://surecando.com.au/wp-content/uploads/2023/07/ASUS-LAPTOP-PC-REPAIRS-SURECANDO13.png',
+    title: 'Asus gamer',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 3755,
+  },
+  {
+    id: 6,
+    image:
+      'https://i5.walmartimages.com/seo/ASUS-Laptop-ZenBook-S-Intel-Core-Ultra-7-155U-32GB-Memory-1-TB-PCIe-SSD-Intel-Graphics-13-3-Windows-11-Pro-64-bit-UX5304MA-XS76_d1f59e13-e8e7-4b9a-afb0-23cd03d0642b.e2e4ad5cdaa97d31b3b1ba59a8df5f9f.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF',
+    title: 'Rog desktop',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 2452,
+  },
+  {
+    id: 7,
+    image:
+      'https://i5.walmartimages.com/seo/ASUS-VivoBook-15-Thin-Light-Laptop-15-6-FHD-Display-Intel-i3-1005G1-CPU-8GB-RAM-128GB-SSD-Backlit-Keyboard-Fingerprint-Windows-10-Home-S-Mode-Slate-G_87e31702-21c2-4580-9440-e288feef6ec3.dcb580e93c64ddba84cfc96b2d994c67.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF',
+    title: 'MSI play',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 1478,
+  },
+  {
+    id: 8,
+    image:
+      'https://i5.walmartimages.com/seo/ASUS-Vivobook-S-16-Flip-16-inch-Touch-2-in-1-Laptop-Intel-Core-i5-1335U-8GB-RAM-512GB-SSD-Midnight-Black_3f0c7cf1-7f2c-49db-b7cd-d0496e1fa119.a33aaed573f7593128737eb742022146.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF',
+    title: 'Think pad',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 899,
+  },
+]
