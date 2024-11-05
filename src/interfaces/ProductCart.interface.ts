48954b808927653d714a6165edaa9aafc0d0928b
CedricNtwari <cedric.ntwari@powercoders.org>
2024-11-05 15:33:25 +0100

    fix(interfaces): correct export path for Product.interface

--- src/interfaces/ProductCart.interface.ts
+++ src/interfaces/ProductCart.interface.ts
@@ -1,4 +1,4 @@
-import type { ProductInterface } from './ProductTmp.interface'
+import type { ProductInterface } from './Product.interface'
 
 export interface ProductCartInterface extends ProductInterface {
   quantity: number
