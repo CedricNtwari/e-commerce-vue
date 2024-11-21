5eb60182bd37b98c6c599daa240922ab02113cdf
CedricNtwari <cedric.ntwari@powercoders.org>
2024-11-21 08:24:22 +0100

    feat(router): add NotFound route for handling unmatched paths

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="container d-flex flex-row justify-content-center align-items-center"
+  >
+    <h1>Page Not found! Please go back to homepage!</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  height: 100%;
+}
+</style>
